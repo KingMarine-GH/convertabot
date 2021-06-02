6d9a0be4b92402a6461bbe415e7e94c6d25452cc
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-02 13:57:04 +0800

    added respones and did a minor workaround an axios error (name.toUpperCase is not a function)

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ require("dotenv").config();
 
 const Discord = require("discord.js");
 
-const axios = require("axios").default({
-    baseURL: "https://discord.com/api/v8",
-    headers: `BOT ${process.env.TOKEN}`
-});
+const axios = require("axios").default
 
 const client = new Discord.Client();
 
@@ -15,25 +12,30 @@ client.once("ready", () => {
 
 client.on("raw", e => {
     if (e.t == "INTERACTION_CREATE") {
+        console.log("received an interaction")
         // eslint-disable-next-line no-unused-vars
         const { op, d: data, s: seq, t: eventname } = e;
 
         const { name, options } = data.data;
 
         const reply = message => {
-            axios.post(`/interactions/${data.id}/${data.token}/callback`, {
+            axios.post(`https://discord.com/api/v8/interactions/${data.id}/${data.token}/callback`, {
                 type: 4,
                 data: {
                     content: message
+                }
+            }, {
+                headers: {
+                    Authorization: `BOT ${process.env.TOKEN}`
                 }
             });
         };
 
         switch (name) {
             case "convert-temp": {
-                const temp = options[1];
+                const temp = options[1].value;
 
-                switch (options[0]) {
+                switch (options[0].value) {
                     // option 0 is original temp
                     case "C":
                         reply(`${temp}ºC = ${(temp * (9/5)) + 32}ºF`);
